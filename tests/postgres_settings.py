2886b356dee7237a8ebbeb73bc09822c5116ad25
Simon Charette <charette.s@gmail.com>
2012-01-24 01:24:16 -0500

    Removed the last dynamodef reference.

--- tests/postgres_settings.py
+++ tests/postgres_settings.py
@@ -4,9 +4,9 @@ from test_project.settings import * #@UnusedWildImport
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dynamodef',                      # Or path to database file if using sqlite3.
-        'USER': 'dynamodef',                      # Not used with sqlite3.
-        'PASSWORD': 'dynamodef',                  # Not used with sqlite3.
+        'NAME': 'mutant',                      # Or path to database file if using sqlite3.
+        'USER': 'mutant',                      # Not used with sqlite3.
+        'PASSWORD': 'mutant',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
