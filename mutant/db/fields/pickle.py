df18fabd1275186bade31ce7cc44a408b0b4a735
Simon Charette <charette.s@gmail.com>
2012-02-02 19:57:43 -0500

    Added a missing file for commit 691426d1c6

---
+++ mutant/db/fields/pickle.py
@@ -0,0 +1,22 @@
+
+from picklefield.fields import PickledObjectField as _PickledObjectField
+
+class _ObjectWrapper(object):
+    __slots__ = ('_obj',)
+    
+    def __init__(self, obj):
+        self._obj = obj
+
+class PickledObjectField(_PickledObjectField):
+    
+    def to_python(self, value):
+        value = super(PickledObjectField, self).to_python(value)
+        if isinstance(value, _ObjectWrapper):
+            value = value._obj
+        return value
+    
+    def pre_save(self, model_instance, add):
+        value = super(PickledObjectField, self).pre_save(model_instance, add)
+        if hasattr(value, 'prepare_database_save'):
+            value = _ObjectWrapper(value)
+        return value
