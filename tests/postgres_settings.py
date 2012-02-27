dbef7e7cff437082ae91d0cec276389e6e89ef91
Simon Charette <charette.s@gmail.com>
2012-02-27 17:52:59 -0500

    Added postgres testing for travis

--- tests/postgres_settings.py
+++ tests/postgres_settings.py
@@ -1,13 +1,11 @@
 
-from test_project.settings import * #@UnusedWildImport
+from test_project.settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mutant',                      # Or path to database file if using sqlite3.
-        'USER': 'mutant',                      # Not used with sqlite3.
-        'PASSWORD': 'mutant',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mutant',
+        'USER': 'mutant',
+        'PASSWORD': 'mutant',
     }
 }
