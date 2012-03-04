ce4406056184fa2186875f3373f1157e578aeb78
Simon Charette <charette.s@gmail.com>
2012-03-03 19:11:46 -0500

    Fixed the multi-db support to rely on router to determine which db to run DDL operations on. This should allow storing all MutableModels on a different database.

--- mutant/management/__init__.py
+++ mutant/management/__init__.py
@@ -1,6 +1,6 @@
 
 from django.contrib.contenttypes.models import ContentType
-from django.db import models
+from django.db import models, router
 from django.db.models.fields import FieldDoesNotExist
 from django.db.models.signals import m2m_changed, post_delete, post_save, pre_delete
 from south.db import dbs
@@ -10,11 +10,13 @@ from mutant.models import (ModelDefinition, BaseDefinition, FieldDefinition,
 
 
 def model_definition_post_save(sender, instance, created, raw, **kwargs):
-    db = instance._state.db
     if raw:
-        ct = ContentType.objects.using(db).get(pk=instance.pk)
+        ct_db = instance._state.db # cts should be one the same db as mds
+        ct = ContentType.objects.using(ct_db).get(pk=instance.pk)
         instance.app_label, instance.model = ct.app_label, ct.model
-    opts = instance.model_class(force_create=True)._meta
+    model_class = instance.model_class(force_create=True)
+    opts = model_class._meta
+    db = router.db_for_write(model_class)
     if created:
         fields = tuple((field.name, field) for field in opts.fields)
         dbs[db].create_table(opts.db_table, fields)
@@ -30,8 +32,9 @@ post_save.connect(model_definition_post_save, ModelDefinition,
 
 def model_definition_post_delete(sender, instance, **kwargs):
     model_class = instance.model_class()
+    db = router.db_for_write(model_class)
     db_table = model_class._meta.db_table
-    dbs[instance._state.db].delete_table(db_table)
+    dbs[db].delete_table(db_table)
     
 post_delete.connect(model_definition_post_delete, ModelDefinition,
                     dispatch_uid='mutant.management.model_definition_post_delete')
@@ -39,21 +42,21 @@ post_delete.connect(model_definition_post_delete, ModelDefinition,
 def base_definition_post_save(sender, instance, created, raw, **kwargs):
     base = instance.base
     if issubclass(base, models.Model):
-        db = instance._state.db
-        model = instance.model_def.model_class()
-        opts = model._meta
+        model_class = instance.model_def.model_class()
+        opts = model_class._meta
+        db = router.db_for_write(model_class)
         table_name = opts.db_table
         if created:
             for field in base._meta.fields:
                 dbs[db].add_column(table_name, field.name,
-                                         field, keep_default=False)
+                                   field, keep_default=False)
         else:
             for field in base._meta.fields:
                 try:
                     old_field = opts.get_field(field.name)
                 except FieldDoesNotExist:
                     dbs[db].add_column(table_name, field.name,
-                                             field, keep_default=False)
+                                       field, keep_default=False)
                 else:
                     column = old_field.get_attname_column()[1]
                     dbs[db].alter_column(table_name, column, field)
@@ -62,35 +65,42 @@ post_save.connect(base_definition_post_save, BaseDefinition,
                   dispatch_uid='mutant.management.base_definition_post_save')
 
 def base_definition_pre_delete(sender, instance, **kwargs):
+    """
+    This is used to pass data required for deletion to the post_delete
+    signal that is no more available thereafter.
+    """
     if issubclass(instance.base, models.Model):
-        model = instance.model_def.model_class()
-        instance._deletion_table_name = model._meta.db_table
+        model_class = instance.model_def.model_class()
+        instance._deletion = (
+            router.db_for_write(model_class),
+            model_class._meta.db_table,
+        )
 
 pre_delete.connect(base_definition_pre_delete, BaseDefinition,
                    dispatch_uid='mutant.management.base_definition_pre_delete')
 
 def base_definition_post_delete(sender, instance, **kwargs):
     if issubclass(instance.base, models.Model):
-        table_name = instance._deletion_table_name
-        db = instance._state.db
+        db, table_name = instance._deletion
         for field in instance.base._meta.fields:
             dbs[db].delete_column(table_name, field.name)
-        del instance._deletion_table_name
+        del instance._deletion
 
 post_delete.connect(base_definition_post_delete, BaseDefinition,
                     dispatch_uid='mutant.management.base_definition_post_delete')
 
 def unique_together_field_defs_changed(instance, action, model, **kwargs):
-    names = list(instance.field_defs.names())
-    # If there's no names and action is post_clear there's nothing to do
-    if names and action != 'post_clear':
-        db = instance._state.db
-        db_table = instance.model_def.model_class()._meta.db_table
+    columns = list(instance.field_defs.names())
+    # If there's no columns and action is post_clear there's nothing to do
+    if columns and action != 'post_clear':
+        model_class = instance.model_def.model_class()
+        db = router.db_for_write(model_class)
+        table_name = model_class._meta.db_table
         if action in ('pre_add', 'pre_remove', 'pre_clear'):
-            dbs[db].delete_unique(db_table, names)
+            dbs[db].delete_unique(table_name, columns)
         # Safe guard againts m2m_changed.action api change
         elif action in ('post_add', 'post_remove'):
-            dbs[db].create_unique(db_table, names)
+            dbs[db].create_unique(table_name, columns)
             
 m2m_changed.connect(unique_together_field_defs_changed,
                     UniqueTogetherDefinition.field_defs.through,
@@ -101,10 +111,10 @@ def field_definition_post_save(sender, instance, created, raw, **kwargs):
     This signal is connected by all FieldDefinition subclasses
     see comment in FieldDefinitionBase for more details
     """
-    db = instance._state.db
+    model_class = instance.model_def.model_class()
+    db = router.db_for_write(model_class)
+    table_name = model_class._meta.db_table
     field = instance._south_ready_field_instance()
-    model = instance.model_def.model_class()
-    table_name = model._meta.db_table
     __, column = field.get_attname_column()
     
     if created:
@@ -131,22 +141,22 @@ def field_definition_pre_delete(sender, instance, **kwargs):
     This signal is connected by all FieldDefinition subclasses
     see comment in FieldDefinitionBase for more details
     """
-    opts = instance.model_def.model_class()._meta
-    table_name = opts.db_table
-    column_name = opts.get_field(instance.name).column
-    instance._deletion_table_name = table_name
-    instance._deletion_column_name = column_name
+    model_class = instance.model_def.model_class()
+    opts = model_class._meta
+    instance._deletion = (
+        router.db_for_write(model_class),
+        opts.db_table,
+        opts.get_field(instance.name).column
+    )
 
 def field_definition_post_delete(sender, instance, **kwargs):
     """
     This signal is connected by all FieldDefinition subclasses
     see comment in FieldDefinitionBase for more details
     """
-    db = instance._state.db
-    dbs[db].delete_column(instance._deletion_table_name,
-                          instance._deletion_column_name)
-    del instance._deletion_table_name
-    del instance._deletion_column_name
+    db, table_name, name = instance._deletion
+    dbs[db].delete_column(table_name, name)
+    del instance._deletion
     
 def connect_field_definition(definition):
     model = definition._meta.object_name.lower()
