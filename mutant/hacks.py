64fa07fbed4ed1a882ba4870f79bdcf926b7e450
Simon Charette <charette.s@gmail.com>
2012-03-07 14:29:37 -0500

    Added a ticket reference to the Field compare hack

--- mutant/hacks.py
+++ mutant/hacks.py
@@ -1,9 +1,9 @@
 
-# TODO: Add django ticket number in order to remove this eventually
 # Field instances cannot be compared to other objects because of attribute
 # presence assumptions in it's __cmp__ method. To allow full_clean we must
 # override the item_field __cmp__ method to return NotImplemented if the
-# object it's compared to isn't a Field instance. Let's monkey patch
+# object it's compared to isn't a Field instance. Let's monkey patch it!
+# see https://code.djangoproject.com/ticket/17851
 from django.db.models.fields import Field
 try:
     assert Field() != None
