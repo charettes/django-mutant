1736c31cc808919cea9c9ded651d43c84c81f1d9
Simon Charette <charette.s@gmail.com>
2012-03-07 14:36:48 -0500

    Make sure hacks are only loaded when required

--- mutant/hacks.py
+++ mutant/hacks.py
@@ -4,13 +4,14 @@
 # override the item_field __cmp__ method to return NotImplemented if the
 # object it's compared to isn't a Field instance. Let's monkey patch it!
 # see https://code.djangoproject.com/ticket/17851
-from django.db.models.fields import Field
-try:
-    assert Field() != None
-except AttributeError:
-    def _Field__cmp__(self, other):
-        if isinstance(other, Field):
-            return cmp(self.creation_counter, other.creation_counter)
-        return NotImplemented
-    Field.__cmp__ = _Field__cmp__
-    assert Field() != None
+def patch_db_field_compare():
+    from django.db.models.fields import Field
+    try:
+        assert Field() != None
+    except AttributeError:
+        def _Field__cmp__(self, other):
+            if isinstance(other, Field):
+                return cmp(self.creation_counter, other.creation_counter)
+            return NotImplemented
+        Field.__cmp__ = _Field__cmp__
+        assert Field() != None
