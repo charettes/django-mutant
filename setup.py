d5f24d12b913930e4765f8dc26d5533e69050b26
Simon Charette <charette.s@gmail.com>
2012-03-22 04:03:00 -0400

    Fixed travis tests

--- setup.py
+++ setup.py
@@ -1,44 +1,22 @@
 #!/usr/bin/python
-import os
 import re
 from setuptools import setup, find_packages
 
 from mutant import __version__
 
+
 github_url = 'https://github.com/charettes/django-mutant'
 long_desc = open('README.md').read()
 
-def which(program):
-    """
-    Detect whether or not a program is installed.
-    Thanks to http://stackoverflow.com/a/377028/70191
-    """
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, _ = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ['PATH'].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-EDITABLE_REQUIREMENT = re.compile(r'^-e (?P<link>(?P<vcs>git|svn|hg|bzr).+#egg=(?P<package>.+)-(?P<version>\d(?:\.\d)*))$')
+LINK_REQUIREMENT = re.compile(r'^(?P<link>https?://.+)#egg=(?P<package>.+)$')
 
 install_requires = []
 dependency_links = []
 
 for requirement in (l.strip() for l in open('requirements/stable.txt')):
-    match = EDITABLE_REQUIREMENT.match(requirement)
+    match = LINK_REQUIREMENT.match(requirement)
     if match:
-        assert which(match.group('vcs')) is not None, \
-            "VCS '%(vcs)s' must be installed in order to install %(link)s" % match.groupdict()
-        install_requires.append("%(package)s==%(version)s" % match.groupdict())
+        install_requires.append(match.group('package'))
         dependency_links.append(match.group('link'))
     else:
         install_requires.append(requirement)
