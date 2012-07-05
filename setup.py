263cdad43743546900b88668facb52a13a5aba33
Simon Charette <charette.s@gmail.com>
2012-07-05 10:56:43 -0400

    Simplified the way dependency works.

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python
-import re
 from setuptools import setup, find_packages
 
 from mutant import __version__
 
 github_url = 'https://github.com/charettes/django-mutant'
 long_desc = open('README.md').read()
-
-LINK_REQUIREMENT = re.compile(r'^(?P<link>https://.+#egg=(?P<package>.+)-(?P<version>\d(?:\.\d)*))$')
-
-install_requires = []
-dependency_links = []
-
-for requirement in (l.strip() for l in open('requirements/stable.txt')):
-    match = LINK_REQUIREMENT.match(requirement)
-    if match:
-        install_requires.append("%(package)s==%(version)s" % match.groupdict())
-        dependency_links.append(match.group('link'))
-    else:
-        install_requires.append(requirement)
 
 setup(
     name='django-mutant',
@@ -28,8 +14,15 @@ setup(
     url=github_url,
     author='Simon Charette',
     author_email='charette.s@gmail.com',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=(
+        'django>=1.3,<=1.5',
+        'south>=0.7.5',
+        'django-orderable==1.2.1',
+        'django-picklefield==0.2.0',
+    ),
+    dependency_links=(
+        'https://github.com/tkaemming/django-orderable/tarball/master#egg=django-orderable-1.2.1',
+    ),
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
