446408fcae35c614028191d142b6b75763a7a6cc
Simon Charette <charette.s@gmail.com>
2012-07-23 21:42:18 -0400

    Added a model class attribute descriptor

---
+++ mutant/db/fields/related.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import fields
+from django.db.models.signals import class_prepared
+
+from ...models import ModelDefinition
+from django.db.models.fields import FieldDoesNotExist
+
+
+class ModelClassAttributeDescriptor(object):
+    """
+    Provide an access to an attribute of  a model definition's underlying
+    model class. Useful for defining an accessor to a manager.
+    """
+    def __init__(self, model_def_name, attr_name):
+        self.model_def_name = model_def_name
+        self.attr_name = attr_name
+
+    def __validate(self, **kwargs):
+        opts = self.model._meta
+        try:
+            field = opts.get_field(self.model_def_name)
+        except FieldDoesNotExist:
+            raise ImproperlyConfigured("%s.%s.%s refers to an inexistent field "
+                                       "'%s'" % (opts.app_label, opts.object_name,
+                                                 self.name, self.model_def_name))
+        else:
+            if (not isinstance(field, fields.related.ForeignKey) or
+                not issubclass(field.rel.to, ModelDefinition)):
+                raise ImproperlyConfigured("%s.%s.%s must refer to a ForeignKey "
+                                           "to `ModelDefinition`"
+                                           % (opts.app_label, opts.object_name,
+                                              self.name))
+        setattr(self.model, self.name, self)
+
+    def contribute_to_class(self, cls, name):
+        self.model = cls
+        self.name = name
+        class_prepared.connect(self.__validate, cls, weak=True)
+
+    def __get__(self, instance, instance_type=None):
+        if instance:
+            try:
+                model_def = getattr(instance, self.model_def_name)
+            except ModelDefinition.DoesNotExist:
+                pass
+            else:
+                if model_def is not None:
+                    return getattr(model_def.model_class(), self.attr_name)
+            raise AttributeError("Can't access attribute '%s' of the "
+                                 "model defined by '%s' since it doesn't exist."
+                                 % (self.attr_name, self.model_def_name))
+        else:
+            return self
+
+    def __set__(self, instance, value):
+        raise AttributeError("Can't set attribute")
--- mutant/tests/db/fields.py
+++ mutant/tests/db/fields.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.unittest.case import TestCase
 
+from mutant.db.fields.related import ModelClassAttributeDescriptor
 from mutant.db.fields.translation import LazilyTranslatedField
+from mutant.models import ModelDefinition
+from mutant.tests.models.utils import BaseModelDefinitionTestCase
 
 
 class LazilyTranslatedFieldTest(TestCase):
@@ -25,3 +30,51 @@ class LazilyTranslatedFieldTest(TestCase):
         self.assertEqual(self.field.get_prep_value('hello'), 'hello')
         self.assertEqual(self.field.get_prep_value('hello'), 'hello')
         self.assertEqual(self.field.get_prep_value(1), '1')
+
+
+class ModelWithModelDefinitionReference(models.Model):
+    model_def = models.OneToOneField(ModelDefinition, related_name='+')
+    model_objects = ModelClassAttributeDescriptor('model_def', 'objects')
+
+    nullable_model_def = models.ForeignKey(ModelDefinition,  related_name='+', null=True)
+    nullable_objects = ModelClassAttributeDescriptor('nullable_model_def', 'objects')
+
+    class Meta:
+        app_label = 'mutant'
+
+class ModelDefinitionReferenceTest(BaseModelDefinitionTestCase):
+    def test_manager_name_clash(self):
+        # Inexistent field
+        with self.assertRaises(ImproperlyConfigured):
+            class InexistentModelDefField(models.Model):
+                objs = ModelClassAttributeDescriptor('model_def', 'objects')
+        # Non-FK field
+        with self.assertRaises(ImproperlyConfigured):
+            class NonFKModelDefField(models.Model):
+                name = models.CharField(max_length=100)
+                objs = ModelClassAttributeDescriptor('name', 'objects')
+        # FK not pointing to ModelDefinition
+        with self.assertRaises(ImproperlyConfigured):
+            class NonModelDefFKField(models.Model):
+                model_def = models.ForeignKey('self')
+                objs = ModelClassAttributeDescriptor('model_def', 'objects')
+
+    def test_manager_descriptor(self):
+        obj = ModelWithModelDefinitionReference()
+        # Not nullable field definition should raise
+        with self.assertRaises(AttributeError):
+            obj.model_objects
+        # Nullable field definition should raise
+        with self.assertRaises(AttributeError):
+            obj.nullable_objects
+        # Assigning an existing model def should allow manager retrieval
+        obj.model_def = self.model_def
+        self.assertIsInstance(obj.model_objects, models.Manager)
+        # Assigning an existing model def should allow manager retrieval
+        obj.nullable_model_def = self.model_def
+        self.assertIsInstance(obj.nullable_objects, models.Manager)
+        # Making sure we've got the right model
+        Model = self.model_def.model_class()
+        Model.objects.create()
+        self.assertEqual(obj.model_objects.count(), 1)
+        self.assertEqual(obj.nullable_objects.count(), 1)
