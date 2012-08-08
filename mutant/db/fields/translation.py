bdb1b39255e0c3ad5fc94a19df7796dab57c70c1
Simon Charette <charette.s@gmail.com>
2012-08-08 14:06:37 -0400

    Fixed a breakage due to django 1.5 support for python 3.

--- mutant/db/fields/translation.py
+++ mutant/db/fields/translation.py
@@ -1,24 +1,33 @@
 from __future__ import unicode_literals
 
+import django
 from django.db.models.fields import TextField
 from django.utils.encoding import smart_unicode
 from django.utils.functional import Promise
 from django.utils.translation import ugettext_lazy as _
 
 
-def _is_ugettext_promise(obj):
-    return isinstance(obj, Promise) and (obj._delegate_unicode or
-                                         obj._delegate_str)
+if django.VERSION[0:2] > (1, 4):
+    _delegate_bytes = '_delegate_bytes'
+    _delegate_text = '_delegate_text'
+else:
+    _delegate_bytes = '_delegate_str'
+    _delegate_text = '_delegate_unicode'
+
+def _is_gettext_promise(value):
+    return isinstance(value, Promise) and (getattr(value, _delegate_bytes) or
+                                           getattr(value, _delegate_text))
+
 
 class LazilyTranslatedField(TextField):
     def to_python(self, value):
-        if value is None or _is_ugettext_promise(value):
+        if value is None or _is_gettext_promise(value):
             return value
         return _(smart_unicode(value))
 
     def get_prep_value(self, value):
         if value is None:
             return value
-        elif _is_ugettext_promise(value):
+        elif _is_gettext_promise(value):
             value = smart_unicode(value._proxy____args[0])
         return smart_unicode(value)
