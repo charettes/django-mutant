ee46fc51f8d92c299cabd50feca1633d42b67784
Simon Charette <charette.s@gmail.com>
2012-12-14 16:35:12 -0500

    Moved the attached unsaved bases and fields logic out of ModelDefinitionManager.
    This should allow attaching fields and bases to a
    a model without actually creating it immediatly.

--- mutant/models/model/__init__.py
+++ mutant/models/model/__init__.py
@@ -132,7 +132,30 @@ class ModelDefinition(ContentType):
     natural_key.dependencies = ('contenttypes.contenttype',)
 
     def __init__(self, *args, **kwargs):
+        # Attach unsaved related objects
+        bases = kwargs.pop('bases', ())
+        fields = kwargs.pop('fields', ())
+        extra_fields = []
+        delayed_save = []
+        for base in bases:
+            assert base.pk is None, 'Cannot associate already existing BaseDefinition'
+            extra_fields.extend(
+                [(f.get_attname_column()[1], f) for f in base.get_declared_fields()]
+            )
+            base._state._add_columns = False
+            delayed_save.append(base)
+        for field in fields:
+            assert field.pk is None, 'Cannot associate already existing FieldDefinition'
+            field_instance = field._south_ready_field_instance()
+            extra_fields.append(
+                (field_instance.get_attname_column()[1], field_instance)
+            )
+            field._state._add_column = False
+            delayed_save.append(field)
         super(ModelDefinition, self).__init__(*args, **kwargs)
+        # Add those fields to the instance state to be retrieved later
+        self._state._create_extra_fields = extra_fields
+        self._state._create_delayed_save = delayed_save
         if self.pk:
             self._model_class = super(ModelDefinition, self).model_class()
 
--- mutant/models/model/managers.py
+++ mutant/models/model/managers.py
@@ -8,27 +8,3 @@ class ModelDefinitionManager(models.Manager):
 
     def get_by_natural_key(self, app_label, model):
         return self.get(app_label=app_label, model=model)
-
-    def create(self, bases=(), fields=(), **kwargs):
-        obj = self.model(**kwargs)
-        extra_fields = []
-        delayed_save = []
-        for base in bases:
-            assert base.pk is None, 'Cannot associate already existing BaseDefinition'
-            extra_fields.extend([(f.get_attname_column()[1], f)
-                                 for f in base.get_declared_fields()])
-            base._state._add_columns = False
-            delayed_save.append(base)
-        for field in fields:
-            assert field.pk is None, 'Cannot associate already existing FieldDefinition'
-            field_instance = field._south_ready_field_instance()
-            extra_fields.append((field_instance.get_attname_column()[1], field_instance))
-            field._state._add_column = False
-            delayed_save.append(field)
-        # Add those fields to the instance state to be retrieved later
-        obj._state._create_extra_fields = extra_fields
-        obj._state._create_delayed_save = delayed_save
-        # Force for write
-        self._for_write = True
-        obj.save(force_insert=True, using=self.db)
-        return obj
