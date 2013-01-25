29fcf25cd04f960256748efa896c7a807b8c7d76
Simon Charette <charette.s@gmail.com>
2013-01-25 16:28:57 -0500

    Reoranised requirements, once again, to allow both ci and setup to work correctly

--- setup.py
+++ setup.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
+import os
+import re
 from setuptools import setup, find_packages
 
 from mutant import __version__
 
 
-github_url = 'https://github.com/charettes/django-mutant'
-long_desc = open('README.md').read()
+MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+LINK_REQUIREMENT = re.compile(
+    r'^https://.+#egg=(?P<package>.+)-(?P<version>\d(?:\.\d)*)$'
+)
+
+install_requires = ['django>=1.4']
+dependency_links = []
+
+for requirement in (l.strip() for l in open(os.path.join(MODULE_PATH, 'requirements/base.txt'))):
+    match = LINK_REQUIREMENT.match(requirement)
+    if match:
+        install_requires.append("%(package)s==%(version)s" % match.groupdict())
+        dependency_links.append(match.group())
+    else:
+        install_requires.append(requirement)
 
 setup(
     name='django-mutant',
     version='.'.join(str(v) for v in __version__),
     description='Dynamic model definition and alteration (evolving schemas)',
-    long_description=open('README.md').read(),
-    url=github_url,
+    long_description=open(os.path.join(MODULE_PATH, 'README.md')).read(),
+    url='https://github.com/charettes/django-mutant',
     author='Simon Charette',
     author_email='charette.s@gmail.com',
-    install_requires=(
-        'django>=1.4',
-        'south>=0.7.6',
-        'django-orderable==1.2.1',
-        'django-picklefield>=0.3.0',
-        'django-polymodels>=1.1.0',
-    ),
-    dependency_links=(
-        'https://github.com/tkaemming/django-orderable/tarball/master#egg=django-orderable-1.2.1',
-    ),
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
