cd8409bc33a94445a26a478f780a3ea0ec58fb89
tepez <tomyam1@gmail.com>
2013-03-03 12:45:57 +0200

    Update deletion.py

--- mutant/db/deletion.py
+++ mutant/db/deletion.py
@@ -3,8 +3,8 @@ from django.db.models.deletion import CASCADE
 
 def CASCADE_MARK_ORIGIN(collector, field, sub_objs, using):
     """
-    Custom on_delete handler which sets  cascade_deletion_origin on the _state
-    of the  all relating objects that will deleted.
+    Custom on_delete handler which sets cascade_deletion_origin on the _state
+    of the all relating objects that will deleted.
     We use this handler on ModelDefinitionAttribute.model_def, so when we delete
     a ModelDefinition we can skip field_definition_post_delete and
     base_definition_post_delete and avoid an incremental columns deletion before
