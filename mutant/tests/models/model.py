93cc80fc4a983b6d2011c9e0cf5031b14dd9e8b6
tepez <tomyam1@gmail.com>
2013-03-03 14:19:15 +0200

    Update model.py

--- mutant/tests/models/model.py
+++ mutant/tests/models/model.py
@@ -163,10 +163,21 @@ class ModelDefinitionTest(BaseModelDefinitionTestCase):
     
     def test_model_def_deletion(self):
         model_cls = self.model_def.model_class()
+        self.assertModelTablesExist(model_cls)
+
+        _model_dbs = model_dbs(model_cls)
+        _model_tables = model_cls._meta.db_table
+
         connection = connections[router.db_for_write(model_cls)]
         with CaptureQueriesContext(connection) as captured_queries:
             self.model_def.delete()
+        # ensure that no ALTER queries where issues during deletion of model_def,
+        # that is, check that the table was not deleted on column at a time before
+        # the entire table was dropped.
         self.assertFalse(any('ALTER' in query for query in captured_queries))
+
+        for db in _model_dbs:
+            self.assertTableDoesntExists(db, _model_tables)
 
 
 class ModelDefinitionManagerTest(BaseModelDefinitionTestCase):
