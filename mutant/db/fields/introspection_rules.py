6be22b8f775e074c03f2ef3d6127b5c6b69dd718
tepez <tomyam1@gmail.com>
2013-03-19 13:47:07 +0200

    Create introspection_rules.py

---
+++ mutant/db/fields/introspection_rules.py
@@ -0,0 +1,6 @@
+from south.modelsinspector import add_introspection_rules
+
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.PythonIdentifierField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.RegExpStringField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.DirectoryPathField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.translation\.LazilyTranslatedField'])
