44c0c510a3fd89675ebdd34795bfbccd88bc2718
tepez <tomyam1@gmail.com>
2013-03-19 13:46:49 +0200

    Fixed #13 -- Add south introspection rules for custom fields.

---
+++ mutant/db/fields/introspection_rules.py
@@ -0,0 +1,7 @@
+from south.modelsinspector import add_introspection_rules
+
+
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.PythonIdentifierField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.RegExpStringField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.python\.DirectoryPathField'])
+add_introspection_rules([], ['^mutant\.db\.fields\.translation\.LazilyTranslatedField'])
