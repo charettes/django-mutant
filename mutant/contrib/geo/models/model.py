a8c504af3212eb62269dbb7c97e11f0e651cce2d
Simon Charette <charette.s@gmail.com>
2015-09-01 00:21:24 -0400

    Stopped using the GeoManager class on Django 1.9+.

--- mutant/contrib/geo/models/model.py
+++ mutant/contrib/geo/models/model.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+import django
 from django.contrib.gis.db import models
+
+GeoManager = models.Manager if django.VERSION >= (1, 9) else models.GeoManager
 
 
 class GeoModel(models.Model):
@@ -8,7 +11,7 @@ class GeoModel(models.Model):
     A model to be used as a BaseDefinition on ModelDefinition instance with
     GeometryFieldDefinition instances.
     """
-    objects = models.GeoManager()
+    objects = GeoManager()
 
     class Meta:
         abstract = True
