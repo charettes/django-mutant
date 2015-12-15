e5ff4797b0ccd75b7c85dfb5da864867f98594f2
Simon Charette <charette.s@gmail.com>
2015-12-15 13:29:37 -0500

    Defaulted on_delete to CASCADE for FieldDefinitionTypeField.

--- mutant/db/fields/generic.py
+++ mutant/db/fields/generic.py
@@ -1,14 +1,15 @@
 from __future__ import unicode_literals
 
+from django.db import models
 from polymodels.fields import PolymorphicTypeField
 
 from ... import forms
 
 
 class FieldDefinitionTypeField(PolymorphicTypeField):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, on_delete=models.CASCADE, *args, **kwargs):
         super(FieldDefinitionTypeField, self).__init__(
-            'mutant.FieldDefinition', *args, **kwargs
+            'mutant.FieldDefinition', on_delete=on_delete, *args, **kwargs
         )
 
     def formfield(self, **kwargs):
