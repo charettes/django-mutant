760eab0d6721f8a5e4a996274d8c56fbbd2f1dbe
Simon Charette <charette.s@gmail.com>
2017-01-20 23:56:09 -0500

    Re-allowed bases/fields to be passed to ModelDefinition get/update or create.

--- mutant/models/model/managers.py
+++ mutant/models/model/managers.py
@@ -3,8 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class ModelDefinitionManager(models.Manager):
-    use_for_related_fields = True
+class ModelDefinitionQuerySet(models.QuerySet):
+    def _extract_model_params(self, defaults, **kwargs):
+        # Work around get/update_or_create validations of parameters to allow
+        # bases and fields to be passed to ModelDefinition's initializer.
+        bases = defaults.pop('bases', None)
+        fields = defaults.pop('fields', None)
+        lookup, params = super(ModelDefinitionQuerySet, self)._extract_model_params(defaults, **kwargs)
+        if bases is not None:
+            params['bases'] = bases
+        if fields is not None:
+            params['fields'] = fields
+        return lookup, params
 
     def get_by_natural_key(self, app_label, model):
         return self.get(app_label=app_label, model=model)
+
+
+class ModelDefinitionManager(models.Manager.from_queryset(ModelDefinitionQuerySet)):
+    use_for_related_fields = True
